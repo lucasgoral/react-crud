7a7ecc8f29466be0468e3a4393ec930e611308ff
Łukasz Goral <lukasz.goral.design@gmail.com>
2020-02-01 21:41:38 +0100

    validation fix

--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -17,11 +17,8 @@ const RecipeForm = ({
     ingChanged: false
   };
 
-
-
   const [validation, seValidation] = useState(initialValidation);
   const validate = e => {
-    
     if (e.target.name === "title") {
       seValidation({
         ...validation,
@@ -36,68 +33,80 @@ const RecipeForm = ({
         ingChanged: true
       });
     }
-  
-   
   };
   const validAndHandleCHnage = e => {
     handleChange(e);
     validate(e);
   };
+
+  const submitDisabled = () => {
+    if (edit) {
+      return (
+        (validation.ingChanged || validation.titleChanged) &&
+        validation.title &&
+        validation.ingredients
+      );
+    }
+    return (
+      validation.ingChanged &&
+      validation.titleChanged &&
+      validation.title &&
+      validation.ingredients
+    );
+  };
   return (
     <div>
       <form action="post">
-        <h2>{edit ? "Edit the recipe" : "Add a recipe"}</h2>
+        <h2> {edit ? "Edit the recipe" : "Add a recipe"} </h2>{" "}
         <div>
-          <h3>Recipe title</h3>
+          <h3> Recipe title </h3>{" "}
           <input
             type="text"
             name="title"
             onChange={validAndHandleCHnage}
             value={title}
-          />
-          <p
-          className='error'
-           style={{
-              display:
-                !validation.title && validation.titleChanged ? "block" : "none"
-            }}
-          >
-            Incorrect title
-          </p>
-        </div>
-        <div>
-          <h3>Ingredients</h3>
-          <textarea
-            name="ingredients"
-            onChange={validAndHandleCHnage}
-            value={ingredients}
-          />
+          />{" "}
           <p
             className="error"
             style={{
               display:
-                !validation.ingredients && validation.ingChanged ? "block" : "none"
+                !validation.title && validation.titleChanged ? "block" : "none"
             }}
           >
-            Incorrect ingredients list
-          </p>
-        </div>
+            Incorrect title{" "}
+          </p>{" "}
+        </div>{" "}
+        <div>
+          <h3> Ingredients </h3>{" "}
+          <textarea
+            name="ingredients"
+            onChange={validAndHandleCHnage}
+            value={ingredients}
+          />{" "}
+          <p
+            className="error"
+            style={{
+              display:
+                !validation.ingredients && validation.ingChanged
+                  ? "block"
+                  : "none"
+            }}
+          >
+            Incorrect ingredients list{" "}
+          </p>{" "}
+        </div>{" "}
         <button
           className="bt bt-good"
           type="submit"
           onClick={handleSubmit}
-          disabled={
-            validation.ingChanged && validation.titleChanged && validation.title && validation.ingredients
-              ? false
-              : true
-          }
+          disabled={submitDisabled()}
         >
-          {edit ? "Edit Item" : "Add item"}
-        </button>
+          {edit ? "Edit Item" : "Add item"}{" "}
+        </button>{" "}
         <button type="button" className="bt" onClick={close}>
-          Close
-        </button>
-      </form>
+          Close{" "}
+        </button>{" "}
+      </form>{" "}
     </div>
   );
 };
